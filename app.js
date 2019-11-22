10cab39ee0a53b7cda057747c7239908d8b38774
cyrioram <coram007@gmail.com>
2019-11-21 19:09:08 -0800

    fixed more promps explicitly

--- app.js
+++ app.js
@@ -4,14 +4,14 @@
 
 var userName = prompt('Salutations! What is your name?');
 console.log('The Users name is:', userName);
-alert('Excited to meet you' + userName + '!' + 'Let us see how well you know David through a series of 5 YES and NO questions.')
+alert('Excited to meet you ' + userName + ' !' + ' Let us see how well you know David through a series of 5 YES and NO questions.')
 
 //1st question about fav food + set response UPPERCASE
 
-var favFood = prompt('Is spaghetti David\'s favorite food?').toUpperCase();
-console.log('Faves Spaghetti:', favFood);
+var favFood = prompt('Is spaghetti David\'s favorite food?');
+console.log('Faves Spaghetti:', favFood.toUpperCase());
 
-if (favFood === 'YES' || 'Y') {
+if (favFood === 'YES' || favFood ==='Y') {
   //Provide cofirmative feedback
   alert('You are absolutely right. David loves spaghetti!');
 } else {
@@ -24,7 +24,7 @@ if (favFood === 'YES' || 'Y') {
 var favColor = prompt('Is red David\'s favorite color?').toUpperCase();
 console.log('Favors Red:', favColor);
 
-if (favColor === 'NO' || 'N') {
+if (favColor === 'NO' || favColor === 'N') {
   //Provide cofirmative feedback
   alert('You are absolutely right. David loves the color blue!');
 } else {
@@ -37,7 +37,7 @@ if (favColor === 'NO' || 'N') {
 var favSport = prompt('Is volleyball David\'s favorite sport?').toUpperCase();
 console.log('Favors Volleyball:', favSport);
 
-if (favSport === 'YES' || 'Y') {
+if (favSport === 'YES' || favSport === 'Y') {
   //Provide cofirmative feedback
   alert('You are absolutely right. David enjoys volleyball the most!');
 } else {
@@ -50,7 +50,7 @@ if (favSport === 'YES' || 'Y') {
 var favTeam = prompt('Are the Dallas Cowbowys David\'s favorite NFL Team?').toUpperCase();
 console.log('Favors Baltimore:', favTeam);
 
-if (favTeam === 'YES' || 'Y') {
+if (favTeam === 'YES' || favTeam === 'Y') {
   //Provide negative cofirmative feedback
   alert('You are absolutely incorrect. David enjoys the Baltimore Raven\'s the most!');
 } else {
@@ -64,20 +64,7 @@ if (favTeam === 'YES' || 'Y') {
 var favGenre = prompt('Is country music David\'s favorite music genre').toUpperCase();
 console.log('Favors Country:', favGenre);
 
-if (favGenre === 'YES' || 'Y') {
-  //Provide negative cofirmative feedback
-  alert('You are absolutely incorrect. David enjoys the Hip Hop the most!');
-} else {
-  //Provide positive feedback
-  alert('That\'s correct. David absolutely can\'t stand country.');
-}
-
-//5th question about fav music genre + set response UPPERCASE
-
-var favGenre = prompt('Is country music David\'s favorite music genre').toUpperCase();
-console.log('Favors Country:', favGenre);
-
-if (favGenre === 'YES' || 'Y') {
+if (favGenre === 'YES' || favGenre === 'Y') {
   //Provide negative cofirmative feedback
   alert('You are absolutely incorrect. David enjoys the Hip Hop the most!');
 } else {
@@ -87,22 +74,22 @@ if (favGenre === 'YES' || 'Y') {
 
 //6th Ask User to guess a number 4 times and provide hints
 
-var number;
+var number =0;
 var counter =1;
 
 while(number !== 100 || counter !== 4 ) {
   number = parseInt(prompt('what is David\'s favorite number?'));
 
   if (number < 100) {
-    alert('you guessed too low');
+    alert('You guessed too low!');
     counter++;
 
   } else if (number > 100) {
-    alert('you guessed too high!');
+    alert('You guessed too high!');
     counter++;
 
   }else if (number === NaN || number === null) {
-    alert('please enter a real number');
+    alert('Please enter a real number');
     counter++;
 
   }else (counter === 4) ;
